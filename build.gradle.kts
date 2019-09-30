e180478ef36674674ac950e6341092596ecbc995
kaede <kaede@hexe.net>
2019-09-30 09:59:01 +0900

    仕切り直し！

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+}
+
+group = "logica"
+version = "NaN"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}

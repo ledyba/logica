8b6dd26fa585af54f5c9ad8a32020302f919bb10
Kaede Fujisaki <kaede@hexe.net>
2019-12-12 23:04:50 +0900

    整理

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinTest
+
 plugins {
+    kotlin("jvm") version "1.3.50"
     java
+    application
 }
 
 group = "logica"
@@ -7,12 +13,20 @@ version = "NaN"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
-    testCompile("junit", "junit", "4.12")
+    implementation(kotlin("stdlib"))
+
+    testImplementation("junit", "junit", "4.12")
+    testImplementation(kotlin("test-junit"))
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_12
+}
+
+application {
+    mainClassName = "logica.KtMain"
 }
---
+++ src/main/kotlin/logica/Main.kt
@@ -0,0 +1,8 @@
+package logica
+
+import javax.sound.sampled.AudioFormat
+
+fun main(args : Array<String>) {
+  //val fmt : AudioFormat = AudioFormat();
+  println("hello")
+}

e5efd9b4dfb1bb499cb754072775e8638e6bc833
Kaede Fujisaki <kaede@hexe.net>
2021-11-07 11:28:37 +0900

    become a VST plugin host

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod app;
 use app::App;
 
 fn main() -> anyhow::Result<()> {
-  cpal::available_hosts().into_iter().for_each(|it| println!("{}", it.name()));
   println!("Let's dance!");
   let app = App::default();
   let native_options = eframe::NativeOptions::default();

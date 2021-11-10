ba64f810883a77408c03148af7559f90e2264e50
Kaede Fujisaki <kaede@hexe.net>
2021-11-11 00:03:57 +0900

    Impl LogicaPlugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,33 @@
 pub mod app;
 
+use vst::plugin::{HostCallback, Info, Plugin};
+
+struct LogicaPlugin {
+
+}
+
+impl Default for LogicaPlugin {
+  fn default() -> Self {
+    Self {
+      
+    }
+  }
+}
+
+impl Plugin for LogicaPlugin {
+    fn new(_host: HostCallback) -> Self {
+        Self {
+
+        }
+    }
+
+    fn get_info(&self) -> Info {
+        Info {
+            name: "Logica".to_string(),
+            unique_id: 1145131919, // Used by hosts to differentiate between plugins.
+            ..Default::default()
+        }
+    }
+}
+
+vst::plugin_main!(LogicaPlugin);

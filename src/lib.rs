8ae1ef937fdcbf9b53236d2d71154b923d59d703
Kaede Fujisaki <kaede@hexe.net>
2021-11-11 01:07:22 +0900

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,4 @@
 pub mod app;
+pub mod proxy;
 
-use vst::plugin::{HostCallback, Info, Plugin};
-
-struct LogicaProxy {
-}
-
-impl Default for LogicaProxy {
-  fn default() -> Self {
-    Self {
-
-    }
-  }
-}
-
-impl Plugin for LogicaProxy {
-    fn new(_host: HostCallback) -> Self {
-        Self {
-
-        }
-    }
-
-    fn get_info(&self) -> Info {
-        Info {
-            name: "Logica".to_string(),
-            unique_id: 1145131919, // Used by hosts to differentiate between plugins.
-            ..Default::default()
-        }
-    }
-}
-
-vst::plugin_main!(LogicaProxy);
+vst::plugin_main!(proxy::ProxyPlugin);

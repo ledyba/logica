ee420488b437e1d56630d09be3c193966f96e71f
Kaede Fujisaki <kaede@hexe.net>
2021-11-21 00:29:42 +0900

    use effGetChunk/effSetChunk to store custom dll path.

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,4 +1,4 @@
-use vst::plugin::{Category, HostCallback, Info};
+use vst::plugin::{Category, HostCallback, Info, PluginParameters};
 
 use crate::{editor::Editor};
 use logica_bridge::Plugin;
@@ -6,6 +6,20 @@ use logica_bridge::Plugin;
 pub struct ProxyPlugin {
   host_callback: HostCallback,
   plugin: Option<Box<dyn Plugin>>,
+}
+
+struct ProxyPluginParameters {
+  path: std::sync::Mutex<String>,
+}
+
+impl PluginParameters for ProxyPluginParameters {
+  fn load_preset_data(&self, data: &[u8]) {
+    let mut lock = self.path.lock().expect("Failed to lock");
+    *lock = String::from_utf8(data.to_vec()).expect("Invalid data");
+  }
+  fn get_preset_data(&self) -> Vec<u8> {
+    Vec::from(self.path.lock().expect("Failed to lock").as_bytes())
+  }
 }
 
 impl Default for ProxyPlugin {
@@ -18,13 +32,6 @@ impl Default for ProxyPlugin {
 }
 
 impl vst::plugin::Plugin for ProxyPlugin {
-  fn new(host_callback: HostCallback) -> Self {
-    Self {
-      host_callback,
-      plugin: None,
-    }
-  }
-
   fn get_info(&self) -> Info {
     Info {
       name: "Logica".to_string(),
@@ -32,7 +39,15 @@ impl vst::plugin::Plugin for ProxyPlugin {
       unique_id: 1145131919, // Used by hosts to differentiate between plugins.
       version: 1,
       category: Category::Synth,
+      preset_chunks: true,
       ..Default::default()
+    }
+  }
+
+  fn new(host_callback: HostCallback) -> Self {
+    Self {
+      host_callback,
+      plugin: None,
     }
   }
 

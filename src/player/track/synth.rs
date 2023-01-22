26fda961a55a4c6da439aafd0dd3741d0fb68c01
Kaede Fujisaki <kaede@hexe.net>
2023-01-22 20:04:28 +0900

    move

--- src/player/track/synth.rs
+++ src/player/track/synth.rs
@@ -12,8 +12,8 @@ impl SynthTrack {
 
 impl super::Track for SynthTrack {
   fn play(&mut self, config: &cpal::StreamConfig, buff: &mut [f32], start_idx: usize) {
-    let num_channel = config.channels as usize;
     use std::f64::consts::PI;
+    let num_channel = config.channels as usize;
     let k = PI * 2.0 * self.freq;
     let samples_per_second = config.sample_rate.0 as f64 * num_channel as f64;
     let mut idx = start_idx;

a9aabc4bee5c9d946c133f7792480cb5802cc1f6
Kaede Fujisaki <kaede@hexe.net>
2023-01-22 20:04:07 +0900

    rename

--- src/player/track/synth.rs
+++ src/player/track/synth.rs
@@ -15,10 +15,10 @@ impl super::Track for SynthTrack {
     let num_channel = config.channels as usize;
     use std::f64::consts::PI;
     let k = PI * 2.0 * self.freq;
-    let sample_rate = config.sample_rate.0 as f64 * num_channel as f64;
+    let samples_per_second = config.sample_rate.0 as f64 * num_channel as f64;
     let mut idx = start_idx;
     for chunk in buff.chunks_exact_mut(num_channel) {
-      let v = ((idx as f64 * k / sample_rate).sin() * 0.8) as f32;
+      let v = ((idx as f64 * k / samples_per_second).sin() * 0.8) as f32;
       for out in chunk {
         *out = v;
       }

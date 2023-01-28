08e2426dff8a4fb21d8dac437796640762858281
Kaede Fujisaki <kaede@hexe.net>
2023-01-28 23:02:27 +0900

    UI整理

--- src/editor.rs
+++ src/editor.rs
@@ -28,34 +28,31 @@ impl Editor {
 
 impl eframe::App for Editor {
   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-    egui::panel::CentralPanel::default().show(ctx, |ui| {
-      egui::TopBottomPanel::top("Logica::MenuBar").show(ui.ctx(), |ui| {
-        egui::menu::bar(ui, |ui| {
-          egui::widgets::global_dark_light_mode_switch(ui);
-          ui.menu_button("File", |ui| {
-            if ui.add(egui::widgets::Button::new("Exit")).clicked() {
-              self.player.pause().expect("Failed to stop");
-              frame.close();
-            }
-          });
-          ui.menu_button("Logic", |ui| {
-            if ui.button("New Synth Logic").clicked() {
-              self.tree.push_to_focused_leaf(Tab::new_synth_tab(self.player.clone()));
-              ui.close_menu();
-            }
-          });
+    egui::TopBottomPanel::top("Logica::MenuBar").show(ctx, |ui| {
+      egui::menu::bar(ui, |ui| {
+        egui::widgets::global_dark_light_mode_switch(ui);
+        ui.menu_button("File", |ui| {
+          if ui.add(egui::widgets::Button::new("Exit")).clicked() {
+            self.player.pause().expect("Failed to stop");
+            frame.close();
+          }
+        });
+        ui.menu_button("Logic", |ui| {
+          if ui.button("New Synth Logic").clicked() {
+            self.tree.push_to_focused_leaf(Tab::new_synth_tab(self.player.clone()));
+            ui.close_menu();
+          }
         });
       });
-
-      egui::CentralPanel::default().show(ui.ctx(), |_ui| {
-        let layer_id = egui::LayerId::background();
-        let max_rect = ctx.available_rect();
-        let clip_rect = ctx.available_rect();
-        let id = egui::Id::new("egui_dock::DockArea");
-        let mut ui = egui::Ui::new(ctx.clone(), layer_id, id, max_rect, clip_rect);
-        egui_dock::DockArea::new(&mut self.tree)
-          .show_inside(&mut ui, &mut TabViewer::new());
-      });
+    });
+    egui::panel::CentralPanel::default().show(ctx, |ui| {
+      let layer_id = egui::LayerId::background();
+      let max_rect = ctx.available_rect();
+      let clip_rect = ctx.available_rect();
+      let id = egui::Id::new("egui_dock::DockArea");
+      let mut ui = egui::Ui::new(ctx.clone(), layer_id, id, max_rect, clip_rect);
+      egui_dock::DockArea::new(&mut self.tree)
+        .show_inside(&mut ui, &mut TabViewer::new());
     });
   }
 }

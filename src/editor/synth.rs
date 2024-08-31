e663fd88be2588d8aec46f3e6e5c5b96edf18c63
Kaede Fujisaki <kaede@hexe.net>
2024-08-31 14:00:49 +0900

    コンパイルは通った

--- src/editor/synth.rs
+++ src/editor/synth.rs
@@ -1,12 +1,75 @@
-pub struct SynthEditor {
-  
+use eframe::egui;
+use egui_snarl::{ui::{SnarlStyle, SnarlViewer}, Snarl};
+
+pub struct NodeEnum {
+
 }
+
+pub struct SynthViewer {
+}
+
+pub struct SynthEditor {
+  viewer: SynthViewer,
+  snarl: Snarl<NodeEnum>,
+  snarl_style: SnarlStyle,
+}
+
+impl SnarlViewer<NodeEnum> for SynthViewer {
+    fn title(&mut self, node: &NodeEnum) -> String {
+      "Synth".to_string()
+    }
+
+    fn outputs(&mut self, node: &NodeEnum) -> usize {
+      0
+    }
+
+    fn inputs(&mut self, node: &NodeEnum) -> usize {
+      0
+    }
+
+    fn show_input(
+      &mut self,
+      pin: &egui_snarl::InPin,
+      ui: &mut eframe::egui::Ui,
+      scale: f32,
+      snarl: &mut Snarl<NodeEnum>
+    ) -> egui_snarl::ui::PinInfo {
+      egui_snarl::ui::PinInfo::star()
+    }
+
+    fn show_output(
+      &mut self,
+      pin: &egui_snarl::OutPin,
+      ui: &mut eframe::egui::Ui,
+      scale: f32,
+      snarl: &mut Snarl<NodeEnum>,
+    ) -> egui_snarl::ui::PinInfo {
+      egui_snarl::ui::PinInfo::star()
+    }
+}
+
+impl SynthViewer {
+  pub fn new() -> Self {
+    Self {
+    }
+  }
+}
+
 impl SynthEditor {
     pub fn new() -> Self {
       Self {
+        viewer: SynthViewer::new(),
+        snarl: Default::default(),
+        snarl_style: SnarlStyle::new(),
       }
     }
-    
-    pub fn ui(&self, ui: &mut eframe::egui::Ui) {
+
+    pub fn ui(&mut self, ui: &mut eframe::egui::Ui) {
+      self.snarl.show(
+        &mut self.viewer,
+        &self.snarl_style,
+        egui::Id::new("snarl"),
+        ui
+      )
     }
 }

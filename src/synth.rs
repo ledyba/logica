362d9a3c10b3694cc37ec38fc624e3f590665053
Kaede Fujisaki <kaede@hexe.net>
2024-08-31 23:37:44 +0900

    Remove unused

--- src/synth.rs
+++ src/synth.rs
@@ -1,21 +1,16 @@
 pub struct Synth {
-  pub freq: f64,
 }
 
 impl Synth {
   pub fn new(
-    freq: f64,
   ) -> Self {
     Self {
-      freq,
     }
   }
 }
 
 impl Default for Synth {
   fn default() -> Self {
-    Self::new(
-      440.0,
-    )
+    Self::new()
   }
 }

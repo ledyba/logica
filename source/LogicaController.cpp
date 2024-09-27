b720324060ad30779b485ed5f5ede23373281dcf
Kaede Fujisaki <kaede@hexe.net>
2024-09-27 11:31:50 +0900

    Use spaces

--- source/LogicaController.cpp
+++ source/LogicaController.cpp
@@ -15,67 +15,67 @@ namespace Logica {
 //------------------------------------------------------------------------
 tresult PLUGIN_API LogicaController::initialize (FUnknown* context)
 {
-	// Here the Plug-in will be instantiated
+  // Here the Plug-in will be instantiated
 
-	//---do not forget to call parent ------
-	tresult result = EditControllerEx1::initialize (context);
-	if (result != kResultOk)
-	{
-		return result;
-	}
+  //---do not forget to call parent ------
+  tresult result = EditControllerEx1::initialize (context);
+  if (result != kResultOk)
+  {
+    return result;
+  }
 
-	// Here you could register some parameters
+  // Here you could register some parameters
 
-	return result;
+  return result;
 }
 
 //------------------------------------------------------------------------
 tresult PLUGIN_API LogicaController::terminate ()
 {
-	// Here the Plug-in will be de-instantiated, last possibility to remove some memory!
+  // Here the Plug-in will be de-instantiated, last possibility to remove some memory!
 
-	//---do not forget to call parent ------
-	return EditControllerEx1::terminate ();
+  //---do not forget to call parent ------
+  return EditControllerEx1::terminate ();
 }
 
 //------------------------------------------------------------------------
 tresult PLUGIN_API LogicaController::setComponentState (IBStream* state)
 {
-	// Here you get the state of the component (Processor part)
-	if (!state)
-		return kResultFalse;
+  // Here you get the state of the component (Processor part)
+  if (!state)
+    return kResultFalse;
 
-	return kResultOk;
+  return kResultOk;
 }
 
 //------------------------------------------------------------------------
 tresult PLUGIN_API LogicaController::setState (IBStream* state)
 {
-	// Here you get the state of the controller
+  // Here you get the state of the controller
 
-	return kResultTrue;
+  return kResultTrue;
 }
 
 //------------------------------------------------------------------------
 tresult PLUGIN_API LogicaController::getState (IBStream* state)
 {
-	// Here you are asked to deliver the state of the controller (if needed)
-	// Note: the real state of your plug-in is saved in the processor
+  // Here you are asked to deliver the state of the controller (if needed)
+  // Note: the real state of your plug-in is saved in the processor
 
-	return kResultTrue;
+  return kResultTrue;
 }
 
 //------------------------------------------------------------------------
 IPlugView* PLUGIN_API LogicaController::createView (FIDString name)
 {
-	// Here the Host wants to open your editor (if you have one)
-	if (FIDStringsEqual (name, Vst::ViewType::kEditor))
-	{
-		// create your editor here and return a IPlugView ptr of it
-		auto* view = new VSTGUI::VST3Editor (this, "view", "myplugineditor.uidesc");
-		return view;
-	}
-	return nullptr;
+  // Here the Host wants to open your editor (if you have one)
+  if (FIDStringsEqual (name, Vst::ViewType::kEditor))
+  {
+    // create your editor here and return a IPlugView ptr of it
+    auto* view = new VSTGUI::VST3Editor (this, "view", "myplugineditor.uidesc");
+    return view;
+  }
+  return nullptr;
 }
 
 //------------------------------------------------------------------------
--- source/LogicaEntry.cpp
+++ source/LogicaEntry.cpp
@@ -19,32 +19,32 @@ using namespace Logica;
 //------------------------------------------------------------------------
 
 BEGIN_FACTORY_DEF ("My Plug-in Company", 
-			       "https://www.mycompanyname.com", 
-			       "mailto:info@mycompanyname.com")
+             "https://www.mycompanyname.com", 
+             "mailto:info@mycompanyname.com")
 
-	//---First Plug-in included in this factory-------
-	// its kVstAudioEffectClass component
-	DEF_CLASS2 (INLINE_UID_FROM_FUID(kLogicaProcessorUID),
-				PClassInfo::kManyInstances,	// cardinality
-				kVstAudioEffectClass,	// the component category (do not changed this)
-				stringPluginName,		// here the Plug-in name (to be changed)
-				Vst::kDistributable,	// means that component and controller could be distributed on different computers
-				LogicaVST3Category, // Subcategory for this Plug-in (to be changed)
-				FULL_VERSION_STR,		// Plug-in version (to be changed)
-				kVstVersionString,		// the VST 3 SDK version (do not changed this, use always this define)
-				LogicaProcessor::createInstance)	// function pointer called when this component should be instantiated
+  //---First Plug-in included in this factory-------
+  // its kVstAudioEffectClass component
+  DEF_CLASS2 (INLINE_UID_FROM_FUID(kLogicaProcessorUID),
+        PClassInfo::kManyInstances,  // cardinality
+        kVstAudioEffectClass,  // the component category (do not changed this)
+        stringPluginName,    // here the Plug-in name (to be changed)
+        Vst::kDistributable,  // means that component and controller could be distributed on different computers
+        LogicaVST3Category, // Subcategory for this Plug-in (to be changed)
+        FULL_VERSION_STR,    // Plug-in version (to be changed)
+        kVstVersionString,    // the VST 3 SDK version (do not changed this, use always this define)
+        LogicaProcessor::createInstance)  // function pointer called when this component should be instantiated
 
-	// its kVstComponentControllerClass component
-	DEF_CLASS2 (INLINE_UID_FROM_FUID (kLogicaControllerUID),
-				PClassInfo::kManyInstances, // cardinality
-				kVstComponentControllerClass,// the Controller category (do not changed this)
-				stringPluginName "Controller",	// controller name (could be the same than component name)
-				0,						// not used here
-				"",						// not used here
-				FULL_VERSION_STR,		// Plug-in version (to be changed)
-				kVstVersionString,		// the VST 3 SDK version (do not changed this, use always this define)
-				LogicaController::createInstance)// function pointer called when this component should be instantiated
+  // its kVstComponentControllerClass component
+  DEF_CLASS2 (INLINE_UID_FROM_FUID (kLogicaControllerUID),
+        PClassInfo::kManyInstances, // cardinality
+        kVstComponentControllerClass,// the Controller category (do not changed this)
+        stringPluginName "Controller",  // controller name (could be the same than component name)
+        0,            // not used here
+        "",            // not used here
+        FULL_VERSION_STR,    // Plug-in version (to be changed)
+        kVstVersionString,    // the VST 3 SDK version (do not changed this, use always this define)
+        LogicaController::createInstance)// function pointer called when this component should be instantiated
 
-	//----for others Plug-ins contained in this factory, put like for the first Plug-in different DEF_CLASS2---
+  //----for others Plug-ins contained in this factory, put like for the first Plug-in different DEF_CLASS2---
 
 END_FACTORY

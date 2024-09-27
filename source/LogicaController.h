b5240c2ec9c9c22ef418b6c014e5bd6b3d185b2c
Kaede Fujisaki <kaede@hexe.net>
2024-09-27 11:40:14 +0900

    fmt

--- source/LogicaController.h
+++ source/LogicaController.h
@@ -19,27 +19,26 @@ public:
   ~LogicaController () SMTG_OVERRIDE = default;
 
     // Create function
-  static Steinberg::FUnknown* createInstance (void* /*context*/)
-  {
+  static Steinberg::FUnknown* createInstance (void* /*context*/) {
     return (Steinberg::Vst::IEditController*)new LogicaController;
   }
 
   //--- from IPluginBase -----------------------------------------------
-  Steinberg::tresult PLUGIN_API initialize (Steinberg::FUnknown* context) SMTG_OVERRIDE;
-  Steinberg::tresult PLUGIN_API terminate () SMTG_OVERRIDE;
+  Steinberg::tresult PLUGIN_API initialize(Steinberg::FUnknown* context) SMTG_OVERRIDE;
+  Steinberg::tresult PLUGIN_API terminate() SMTG_OVERRIDE;
 
   //--- from EditController --------------------------------------------
-  Steinberg::tresult PLUGIN_API setComponentState (Steinberg::IBStream* state) SMTG_OVERRIDE;
-  Steinberg::IPlugView* PLUGIN_API createView (Steinberg::FIDString name) SMTG_OVERRIDE;
-  Steinberg::tresult PLUGIN_API setState (Steinberg::IBStream* state) SMTG_OVERRIDE;
-  Steinberg::tresult PLUGIN_API getState (Steinberg::IBStream* state) SMTG_OVERRIDE;
+  Steinberg::tresult PLUGIN_API setComponentState(Steinberg::IBStream* state) SMTG_OVERRIDE;
+  Steinberg::IPlugView* PLUGIN_API createView(Steinberg::FIDString name) SMTG_OVERRIDE;
+  Steinberg::tresult PLUGIN_API setState(Steinberg::IBStream* state) SMTG_OVERRIDE;
+  Steinberg::tresult PLUGIN_API getState(Steinberg::IBStream* state) SMTG_OVERRIDE;
 
    //---Interface---------
   DEFINE_INTERFACES
     // Here you can add more supported VST3 interfaces
     // DEF_INTERFACE (Vst::IXXX)
-  END_DEFINE_INTERFACES (EditController)
-    DELEGATE_REFCOUNT (EditController)
+  END_DEFINE_INTERFACES(EditController)
+    DELEGATE_REFCOUNT(EditController)
 
 //------------------------------------------------------------------------
 protected:

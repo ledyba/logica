bc661b2aa303bf5ef0c125cafcdda98f33c6f546
Kaede Fujisaki <kaede@hexe.net>
2024-10-02 18:57:05 +0900

    rename

--- src/LogicaCIDs.h
+++ src/LogicaCIDs.h
@@ -7,7 +7,7 @@
 #include "pluginterfaces/base/funknown.h"
 #include "pluginterfaces/vst/vsttypes.h"
 
-namespace Logica {
+namespace logica {
 //------------------------------------------------------------------------
 static const Steinberg::FUID kLogicaProcessorUID(0xBFF7043C, 0xE3DA507D, 0xB73DDEDD, 0x2F77C358);
 static const Steinberg::FUID kLogicaControllerUID(0xA1038D20, 0x45B450FC, 0xB5CE9F1C, 0x43448F81);

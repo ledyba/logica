ca201d92e3bf342810f8df5a380fea64e63bdf78
Kaede Fujisaki <kaede@hexe.net>
2024-10-16 17:50:45 +0900

    Disable log

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 rmdir /S /Q "C:\\Program Files\\Common Files\\vst3\\ledyba\\Logica.vst3\\"
-robocopy /mir "C:\\Users\\kaede\src\\code.ledyba.org\\ledyba\\logica\\cmake-build-debug-visual-studio\\VST3\\Debug\\Logica.vst3\\" "C:\\Program Files\\Common Files\\vst3\\ledyba\\Logica.vst3\\" /log+:install.log
+robocopy /mir "C:\\Users\\kaede\src\\code.ledyba.org\\ledyba\\logica\\cmake-build-debug-visual-studio\\VST3\\Debug\\Logica.vst3\\" "C:\\Program Files\\Common Files\\vst3\\ledyba\\Logica.vst3\\"

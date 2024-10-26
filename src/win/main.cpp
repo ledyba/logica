2f8ca0528adb287f9c4c555fe7d2eb5c79c5b37d
Kaede Fujisaki <kaede@hexe.net>
2024-10-27 04:59:12 +0900

    DPI awareness

--- src/win/main.cpp
+++ src/win/main.cpp
@@ -41,6 +41,7 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 }
 
 int main(int, char**) {
+  ImGui_ImplWin32_EnableDpiAwareness();
   WNDCLASSEXW wc = {
       sizeof(wc),
       CS_CLASSDC | CS_HREDRAW,
